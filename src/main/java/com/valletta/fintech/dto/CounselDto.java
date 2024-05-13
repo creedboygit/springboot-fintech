196cc7ac16396412e4596fdb6ae57d6c0e66bac9
creedboy <polozhzh@gmail.com>
2024-05-13 21:56:10 +0900

    상담 등록 시 Request, Response Dto 추가

---
+++ src/main/java/com/valletta/fintech/dto/CounselDto.java
@@ -0,0 +1,49 @@
+package com.valletta.fintech.dto;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+public class CounselDto {
+
+    @ToString
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @Getter
+    public static class Request {
+
+        private String name;
+        private String cellPhone;
+        private String email;
+        private String memo;
+        private String address;
+        private String addressDetail;
+        private String zipCode;
+    }
+
+    @ToString
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @Getter
+//    @Setter
+    public static class Response {
+
+        private Long counselId;
+        private String name;
+        private String cellPhone;
+        private String email;
+        private String memo;
+        private String address;
+        private String addressDetail;
+        private String zipCode;
+        private LocalDateTime appliedAt;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+    }
+}
