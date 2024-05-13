22d29cccace182cbfb6efefeeb7443fee50ead4a
creedboy <polozhzh@gmail.com>
2024-05-13 22:10:08 +0900

    import optimize

--- src/main/java/com/valletta/fintech/config/ModelMapperConfig.java
+++ src/main/java/com/valletta/fintech/config/ModelMapperConfig.java
@@ -3,7 +3,6 @@ package com.valletta.fintech.config;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.config.Configuration.AccessLevel;
 import org.modelmapper.convention.MatchingStrategies;
-import org.modelmapper.spi.MatchingStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
