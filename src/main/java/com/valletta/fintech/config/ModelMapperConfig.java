e24b2c62a6d41ab94cb3525b27497656969f48b9
creedboy <polozhzh@gmail.com>
2024-05-13 21:56:59 +0900

    ModelMapper Config 수정 - PRIVATE 필드에도 접근할 수 있도록 수정

---
+++ src/main/java/com/valletta/fintech/config/ModelMapperConfig.java
@@ -0,0 +1,27 @@
+package com.valletta.fintech.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration.AccessLevel;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.spi.MatchingStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+
+//        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+//        modelMapper.getConfiguration().setSkipNullEnabled(true);
+
+        modelMapper.getConfiguration()
+            .setSkipNullEnabled(true)
+            .setMatchingStrategy(MatchingStrategies.STRICT)
+            .setFieldAccessLevel(AccessLevel.PRIVATE)
+            .setFieldMatchingEnabled(true);
+        return modelMapper;
+    }
+}
