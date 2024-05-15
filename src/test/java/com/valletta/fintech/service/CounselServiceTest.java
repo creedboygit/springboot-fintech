e32a41c9e37f678496a5b172d8efbda30b3a97e3
creedboy <polozhzh@gmail.com>
2024-05-15 21:53:29 +0900

    상담 삭제 서비스 테스트 메서드 추가

--- src/test/java/com/valletta/fintech/service/CounselServiceTest.java
+++ src/test/java/com/valletta/fintech/service/CounselServiceTest.java
@@ -114,4 +114,22 @@ class CounselServiceTest {
         assertThat(actual.getCounselId()).isSameAs(findId);
         assertThat(actual.getName()).isSameAs(request.getName());
     }
+
+    @Test
+    void Should_DeletedCounselEntity_When_RequesDeleteExistCounselInfo() {
+
+        Long targetId = 1L;
+
+        Counsel entity = Counsel.builder()
+            .counselId(1L)
+            .build();
+
+        when(counselRepository.save(any(Counsel.class))).thenReturn(entity);
+        when(counselRepository.findById(targetId)).thenReturn(Optional.ofNullable(entity));
+
+        counselService.delete(targetId);
+
+        assert entity != null;
+        assertThat(entity.getIsDeleted()).isSameAs(true);
+    }
 }
