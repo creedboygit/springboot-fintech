6eba7af6729b925c765a64952649e4400e1730ae
creedboy <polozhzh@gmail.com>
2024-05-16 21:33:20 +0900

    대출 신청 컨트롤러 메서드 추가

---
+++ src/main/java/com/valletta/fintech/controller/ApplicationController.java
@@ -0,0 +1,25 @@
+package com.valletta.fintech.controller;
+
+import com.valletta.fintech.dto.ApplicationDto.Request;
+import com.valletta.fintech.dto.ApplicationDto.Response;
+import com.valletta.fintech.dto.ResponseDto;
+import com.valletta.fintech.service.ApplicationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/applications")
+public class ApplicationController extends AbstractController {
+
+    private final ApplicationService applicationService;
+
+    @PostMapping
+    public ResponseDto<Response> create(@RequestBody Request request) {
+
+        return ok(applicationService.create(request));
+    }
+}
