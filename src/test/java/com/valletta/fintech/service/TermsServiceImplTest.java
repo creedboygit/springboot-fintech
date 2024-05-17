ec3169a5fc30480b869e779027be4a27bf7d750b
creedboy <polozhzh@gmail.com>
2024-05-17 20:52:30 +0900

    대출 신청 약관 전체 조회 Service 테스트 추가

--- src/test/java/com/valletta/fintech/service/TermsServiceImplTest.java
+++ src/test/java/com/valletta/fintech/service/TermsServiceImplTest.java
@@ -9,6 +9,9 @@ import com.valletta.fintech.domain.Terms;
 import com.valletta.fintech.dto.TermsDto.Request;
 import com.valletta.fintech.dto.TermsDto.Response;
 import com.valletta.fintech.repository.TermsRepository;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -51,5 +54,28 @@ class TermsServiceImplTest {
         assertThat(actual.getTermsId()).isSameAs(entity.getTermsId());
         assertThat(actual.getName()).isSameAs(entity.getName());
         assertThat(actual.getTermsDetailUrl()).isSameAs(entity.getTermsDetailUrl());
+    }
+
+    @Test
+    void Should_ReturnAllResponseOfExistTermsEntities_When_RequestTermsList() {
+
+        Terms entityA = Terms.builder()
+            .name("대출 이용 약관 1")
+            .termsDetailUrl("https://naver.com/aaa")
+            .build();
+
+        Terms entityB = Terms.builder()
+            .name("대출 이용 약관 2")
+            .termsDetailUrl("https://naver.com/bbb")
+            .build();
+
+        List<Terms> list = new ArrayList<>(List.of(entityA, entityB));
+
+        when(termsRepository.findAll()).thenReturn(list);
+//        when(termsRepository.findAll()).thenReturn(Arrays.asList(entityA, entityB));
+
+        List<Response> actual = termsService.getAll();
+
+        assertThat(actual.size()).isSameAs(list.size());
     }
 }
