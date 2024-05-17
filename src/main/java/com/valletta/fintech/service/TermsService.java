7f1a2f1c20ae9c688473fbd3bfa506c1455097e7
creedboy <polozhzh@gmail.com>
2024-05-17 20:52:23 +0900

    대출 신청 약관 전체 조회 Service 메서드 추가

--- src/main/java/com/valletta/fintech/service/TermsService.java
+++ src/main/java/com/valletta/fintech/service/TermsService.java
@@ -2,8 +2,11 @@ package com.valletta.fintech.service;
 
 import com.valletta.fintech.dto.TermsDto.Request;
 import com.valletta.fintech.dto.TermsDto.Response;
+import java.util.List;
 
 public interface TermsService {
 
     Response create(Request request);
+
+    List<Response> getAll();
 }
--- src/main/java/com/valletta/fintech/service/TermsServiceImpl.java
+++ src/main/java/com/valletta/fintech/service/TermsServiceImpl.java
@@ -1,9 +1,14 @@
 package com.valletta.fintech.service;
 
+import com.valletta.fintech.constant.ResultType;
 import com.valletta.fintech.domain.Terms;
 import com.valletta.fintech.dto.TermsDto.Request;
 import com.valletta.fintech.dto.TermsDto.Response;
+import com.valletta.fintech.exception.BaseException;
 import com.valletta.fintech.repository.TermsRepository;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -21,9 +26,16 @@ public class TermsServiceImpl implements TermsService {
     public Response create(Request request) {
 
         Terms terms = modelMapper.map(request, Terms.class);
-
         Terms created = termsRepository.save(terms);
 
         return modelMapper.map(created, Response.class);
+    }
+
+    @Override
+    public List<Response> getAll() {
+
+        List<Terms> termsList = termsRepository.findAll();
+
+        return termsList.stream().map(t -> modelMapper.map(t, Response.class)).collect(Collectors.toList());
     }
 }
