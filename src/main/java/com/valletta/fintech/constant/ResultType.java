c47d95d6ff32d66b79957915a3158bb1738a204c
creedboy <polozhzh@gmail.com>
2024-05-18 23:17:55 +0900

    파일 정보 조회 시 파일이 없을 경우 에러 코드 추가

--- src/main/java/com/valletta/fintech/constant/ResultType.java
+++ src/main/java/com/valletta/fintech/constant/ResultType.java
@@ -9,6 +9,9 @@ public enum ResultType {
 
     SUCCESS("0000", "success"),
     SYSTEM_ERROR("9000", "system error"),
+
+    // file: 4000 ~ 4999
+    NOT_EXIST("4001", "file not exist"),
     ;
 
     private final String code;
