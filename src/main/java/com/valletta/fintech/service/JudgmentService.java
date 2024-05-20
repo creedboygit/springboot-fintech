718571d7ed6f479ec9b23eadb359057912c6cefd
creedboy <polozhzh@gmail.com>
2024-05-20 21:47:49 +0900

    대출 심사 Service 생성

---
+++ src/main/java/com/valletta/fintech/service/JudgmentService.java
@@ -0,0 +1,9 @@
+package com.valletta.fintech.service;
+
+import com.valletta.fintech.dto.JudgmentDto.Request;
+import com.valletta.fintech.dto.JudgmentDto.Response;
+
+public interface JudgmentService {
+
+    Response create(Request request);
+}
---
+++ src/main/java/com/valletta/fintech/service/JudgmentServiceImpl.java
@@ -0,0 +1,43 @@
+package com.valletta.fintech.service;
+
+import com.valletta.fintech.constant.ResultType;
+import com.valletta.fintech.domain.Judgment;
+import com.valletta.fintech.dto.JudgmentDto.Request;
+import com.valletta.fintech.dto.JudgmentDto.Response;
+import com.valletta.fintech.exception.BaseException;
+import com.valletta.fintech.repository.ApplicationRepository;
+import com.valletta.fintech.repository.JudgmentRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class JudgmentServiceImpl implements JudgmentService {
+
+    private final ApplicationRepository applicationRepository;
+    private final JudgmentRepository judgmentRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public Response create(Request request) {
+
+        Long applicationId = request.getApplicationId();
+
+        // 신청 정보가 있는지 검증
+        if (!isPresentApplication(applicationId)) {
+            throw new BaseException(ResultType.SYSTEM_ERROR);
+        }
+
+        // request dto -> entity -> save
+        Judgment judgment = modelMapper.map(request, Judgment.class);
+        Judgment saved = judgmentRepository.save(judgment);
+
+        // save -> response dto
+        return modelMapper.map(saved, Response.class);
+    }
+
+    private boolean isPresentApplication(Long applicationId) {
+        return applicationRepository.findById(applicationId).isPresent();
+    }
+}
