df3fb2271465e628f405d21c0e94b1729fdf5b2f
creedboy <polozhzh@gmail.com>
2024-05-21 20:45:26 +0900

    대출 심사 정보 조회 Respository 추가

--- src/main/java/com/valletta/fintech/repository/JudgmentRepository.java
+++ src/main/java/com/valletta/fintech/repository/JudgmentRepository.java
@@ -1,10 +1,13 @@
 package com.valletta.fintech.repository;
 
 import com.valletta.fintech.domain.Judgment;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JudgmentRepository extends JpaRepository<Judgment, Long> {
 
+    Optional<Judgment> findByApplicationId(Long applicationId);
 }
