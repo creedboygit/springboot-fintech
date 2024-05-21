67ce0c501c047f399e2ac95dfbb80f574ae3ded6
creedboy <polozhzh@gmail.com>
2024-05-21 19:58:49 +0900

    대출 심사 등록 Controller 메서드 추가

---
+++ src/main/java/com/valletta/fintech/controller/JudgmentController.java
@@ -0,0 +1,25 @@
+package com.valletta.fintech.controller;
+
+import com.valletta.fintech.dto.JudgmentDto.Request;
+import com.valletta.fintech.dto.JudgmentDto.Response;
+import com.valletta.fintech.dto.ResponseDto;
+import com.valletta.fintech.service.JudgmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/judgments")
+public class JudgmentController extends AbstractController {
+
+    private final JudgmentService judgmentService;
+
+    @PostMapping
+    public ResponseDto<Response> create(@RequestBody Request request) {
+
+        return ok(judgmentService.create(request));
+    }
+}
