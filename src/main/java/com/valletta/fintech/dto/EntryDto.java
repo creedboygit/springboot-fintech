f179267f1d41eed71c011e640ef4850d0fe8d33f
creedboy <polozhzh@gmail.com>
2024-05-25 20:24:25 +0900

    대출 집행 Dto 추가

---
+++ src/main/java/com/valletta/fintech/dto/EntryDto.java
@@ -0,0 +1,36 @@
+package com.valletta.fintech.dto;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class EntryDto implements Serializable {
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Request {
+
+        private BigDecimal entryAmount;
+    }
+
+    @Builder
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Response {
+
+        private Long entryId;
+        private Long applicationId;
+        private BigDecimal entryAmount;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+    }
+}
