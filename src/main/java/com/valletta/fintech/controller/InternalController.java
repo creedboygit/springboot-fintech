d67ec38c13652d97c00ffc3ce3c81dbbd9af1720
creedboy <polozhzh@gmail.com>
2024-05-25 20:25:03 +0900

    내부에서 대출 집행하는 컨트롤러 메서드 추가

--- src/main/java/com/valletta/fintech/controller/InternalController.java
+++ src/main/java/com/valletta/fintech/controller/InternalController.java
@@ -1,20 +1,28 @@
 package com.valletta.fintech.controller;
 
+import com.valletta.fintech.dto.EntryDto.Request;
+import com.valletta.fintech.dto.EntryDto.Response;
+import com.valletta.fintech.dto.ResponseDto;
+import com.valletta.fintech.service.EntryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/internal/applications")
+@RequestMapping("/api/v1/internal/applications")
 public class InternalController extends AbstractController {
 
-//    private final EntryService entryService;
+    private final EntryService entryService;
 
 //    private final RepaymentService repaymentService;
 
-//    @PostMapping("{applicationId}/entries")
-//    public ResponseDTO<Response> create(@PathVariable Long applicationId, @RequestBody Request request) {
-//        return ok(entryService.create(applicationId, request));
-//    }
+    @PostMapping("{applicationId}/entries")
+    public ResponseDto<Response> create(@PathVariable Long applicationId, @RequestBody Request request) {
+
+        return ok(entryService.create(applicationId, request));
+    }
 }
