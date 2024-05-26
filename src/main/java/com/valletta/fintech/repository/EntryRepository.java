b60bc70b3cb5ded60783b63952663e33d6b86dfc
creedboy <polozhzh@gmail.com>
2024-05-26 13:50:05 +0900

    대출 집행 정보 조회 Repository 메서드 추가

--- src/main/java/com/valletta/fintech/repository/EntryRepository.java
+++ src/main/java/com/valletta/fintech/repository/EntryRepository.java
@@ -1,10 +1,12 @@
 package com.valletta.fintech.repository;
 
 import com.valletta.fintech.domain.Entry;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EntryRepository extends JpaRepository<Entry, Long> {
 
+    Optional<Entry> findByApplicationId(Long applicationId);
 }
