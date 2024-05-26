b2baf9f153883ce87b9e3b3c21e546f4f504ea10
creedboy <polozhzh@gmail.com>
2024-05-26 14:56:42 +0900

    대출 집행 금액을 업데이트해주는 dto 추가

--- src/main/java/com/valletta/fintech/dto/EntryDto.java
+++ src/main/java/com/valletta/fintech/dto/EntryDto.java
@@ -1,5 +1,7 @@
 package com.valletta.fintech.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -32,5 +34,22 @@ public class EntryDto implements Serializable {
         private BigDecimal entryAmount;
         private LocalDateTime createdAt;
         private LocalDateTime updatedAt;
+    }
+
+    @Builder
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class UpdateResponse {
+
+        private Long entryId;
+        private Long applicationId;
+
+        @JsonFormat(shape = Shape.STRING, pattern = "0")
+        private BigDecimal beforeEntryAmount;
+
+        @JsonFormat(shape = Shape.STRING, pattern = "0")
+        private BigDecimal afterEntryAmount;
     }
 }
