04eeb31c50661e37ff11de2996764095e8f510eb
creedboy <polozhzh@gmail.com>
2024-05-26 14:57:21 +0900

    대출 집행 금액을 업데이트해주는 Controller 메서드 추가

--- src/main/java/com/valletta/fintech/controller/InternalController.java
+++ src/main/java/com/valletta/fintech/controller/InternalController.java
@@ -1,13 +1,16 @@
 package com.valletta.fintech.controller;
 
+import com.valletta.fintech.dto.BalanceDto.UpdateRequest;
 import com.valletta.fintech.dto.EntryDto.Request;
 import com.valletta.fintech.dto.EntryDto.Response;
+import com.valletta.fintech.dto.EntryDto.UpdateResponse;
 import com.valletta.fintech.dto.ResponseDto;
 import com.valletta.fintech.service.EntryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +34,11 @@ public class InternalController extends AbstractController {
     public ResponseDto<Response> get(@PathVariable("applicationId") Long applicationId) {
 
         return ok(entryService.get(applicationId));
+    }
+
+    @PutMapping("/entries/{entryId}")
+    public ResponseDto<UpdateResponse> update(@PathVariable("entryId") Long entryId, @RequestBody Request request) {
+
+        return ok(entryService.update(entryId, request));
     }
 }
