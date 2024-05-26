824bf27b97687fe4e6fc438be74d8a8229137db7
creedboy <polozhzh@gmail.com>
2024-05-26 14:56:13 +0900

    현재 금액을 업데이트해주는 메서드 추가

--- src/main/java/com/valletta/fintech/service/BalanceService.java
+++ src/main/java/com/valletta/fintech/service/BalanceService.java
@@ -2,8 +2,11 @@ package com.valletta.fintech.service;
 
 import com.valletta.fintech.dto.BalanceDto.Request;
 import com.valletta.fintech.dto.BalanceDto.Response;
+import com.valletta.fintech.dto.BalanceDto.UpdateRequest;
 
-public interface BananceService {
+public interface BalanceService {
 
     Response create(Long applicationId, Request request);
+
+    Response update(Long applicationId, UpdateRequest request);
 }
--- src/main/java/com/valletta/fintech/service/BalanceServiceImpl.java
+++ src/main/java/com/valletta/fintech/service/BalanceServiceImpl.java
@@ -4,6 +4,7 @@ import com.valletta.fintech.constant.ResultType;
 import com.valletta.fintech.domain.Balance;
 import com.valletta.fintech.dto.BalanceDto.Request;
 import com.valletta.fintech.dto.BalanceDto.Response;
+import com.valletta.fintech.dto.BalanceDto.UpdateRequest;
 import com.valletta.fintech.exception.BaseException;
 import com.valletta.fintech.repository.BalanceRepository;
 import java.math.BigDecimal;
@@ -13,7 +14,7 @@ import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
-public class BalanceServiceImpl implements BananceService {
+public class BalanceServiceImpl implements BalanceService {
 
     private final BalanceRepository balanceRepository;
     private final ModelMapper modelMapper;
@@ -32,5 +33,24 @@ public class BalanceServiceImpl implements BananceService {
         Balance saved = balanceRepository.save(balance);
 
         return modelMapper.map(saved, Response.class);
+    }
+
+    @Override
+    public Response update(Long applicationId, UpdateRequest request) {
+
+        // balance 찾기
+        Balance balance = balanceRepository.findByApplicationId(applicationId).orElseThrow(() -> new BaseException(ResultType.SYSTEM_ERROR));
+
+        BigDecimal beforeEntryAmount = request.getBeforeEntryAmount();
+        BigDecimal afterEntryAmount = request.getAfterEntryAmount();
+        BigDecimal updatedBalance = balance.getBalance();
+
+        // as-is -> to-be
+        // balance + -
+        updatedBalance = updatedBalance.subtract(beforeEntryAmount).add(afterEntryAmount);
+        balance.updateBalance(updatedBalance);
+        Balance saved = balanceRepository.save(balance);
+
+        return modelMapper.map(balance, Response.class);
     }
 }
