990eee22ea6f58bcb75e414e0fa68406fe4eb6a1
creedboy <polozhzh@gmail.com>
2024-06-04 20:35:03 +0900

    InternalController - repayment 메서드 추가

--- src/main/java/com/valletta/fintech/controller/InternalController.java
+++ src/main/java/com/valletta/fintech/controller/InternalController.java
@@ -6,7 +6,9 @@ import com.valletta.fintech.dto.EntryDto.Response;
 import com.valletta.fintech.dto.EntryDto.UpdateResponse;
 import com.valletta.fintech.dto.ResponseDto;
 import com.valletta.fintech.service.EntryService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,5 +42,27 @@ public class InternalController extends AbstractController {
     public ResponseDto<UpdateResponse> update(@PathVariable("entryId") Long entryId, @RequestBody Request request) {
 
         return ok(entryService.update(entryId, request));
+    }
+
+    @PostMapping("{applicationId}/repayments")
+    public ResponseDTO<RepaymentDTO.Response> create(@PathVariable Long applicationId, @RequestBody RepaymentDTO.Request request) {
+        return ok(repaymentService.create(applicationId, request));
+    }
+
+    @GetMapping("{applicationId}/repayments")
+    public ResponseDTO<List<ListResponse>> getPayments(@PathVariable Long applicationId) {
+        return ok(repaymentService.get(applicationId));
+    }
+
+    @PutMapping("/repayments/{repaymentId}")
+    public ResponseDTO<RepaymentDTO.UpdateResponse> update(@PathVariable Long repaymentId,
+                                                           @RequestBody RepaymentDTO.Request request) {
+        return ok(repaymentService.update(repaymentId, request));
+    }
+
+    @DeleteMapping("/repayments/{repaymentId}")
+    public ResponseDTO<Void> deleteRepayment(@PathVariable Long repaymentId) {
+        repaymentService.delete(repaymentId);
+        return ok();
     }
 }
